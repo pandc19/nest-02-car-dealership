3f5631acd18616f15099effa2176743907c6246d
Jonathan Pillajo <pandc19@outlook.com>
2024-05-26 17:46:23 -0500

    fin sección 5

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -9,21 +9,21 @@ import { CreateCarDto, UpdateCarDto } from './dto';
 export class CarsService {
 
     private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla',
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic',
-        },
-        {
-            id: uuid(),
-            brand: 'Jeep',
-            model: 'Cherokee',
-        }
+        // {
+        //     id: uuid(),
+        //     brand: 'Toyota',
+        //     model: 'Corolla',
+        // },
+        // {
+        //     id: uuid(),
+        //     brand: 'Honda',
+        //     model: 'Civic',
+        // },
+        // {
+        //     id: uuid(),
+        //     brand: 'Jeep',
+        //     model: 'Cherokee',
+        // }
     ];
 
     findAll() {
@@ -76,9 +76,13 @@ export class CarsService {
 
     }
 
-    delete(id: string){
+    delete(id: string) {
         const car = this.findOneById(id);
         this.cars = this.cars.filter(car => car.id !== id);
+    }
+
+    fillCarsWithSeedData(cars: Car[]) {
+        this.cars = cars;
     }
 
 }
